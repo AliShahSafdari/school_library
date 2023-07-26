c65c25104b09553ae3bdc90adc8c85a2093d9ad4
Alishah Safdari <alishahsafdari@gmail.com>
2023-07-26 13:59:17 +0430

    Add trim logic

--- trimmer_decrator.rb
+++ trimmer_decrator.rb
@@ -1,11 +1,8 @@
-# require_relative 'base_decrator'
+require_relative 'base_decrator'
 
 class TrimmerDecorator < BaseDecorator
-  def correct_name
-    if @nameable.correct_name.length < 11
-      @nameable.correct_name
-    else
-      @nameable.correct_name.strip
-    end
-  end
+    def correct_name
+        trimmed_name = @nameable.correct_name.strip
+        trimmed_name.slice(0, 10)
+      end
 end
