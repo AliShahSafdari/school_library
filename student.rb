319a4794dfa1c06fc8b933d2e6c145128a842f62
Alishah Safdari <alishahsafdari@gmail.com>
2023-07-27 14:21:13 +0430

    Add classroom method

--- student.rb
+++ student.rb
@@ -1,10 +1,15 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
+    owner.animals.push(self) unless owner.animals.include?(self)
+  end
+
+  def classroom=(classroom)
     @classroom = classroom
     owner.animals.push(self) unless owner.animals.include?(self)
   end
