7b80d4720a497ebdc1f5ba9281636f790680ebba
Alishah Safdari <alishahsafdari@gmail.com>
2023-08-03 16:18:01 +0430

    Add the test for add_rentals methods

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,6 +3,9 @@ require_relative '../lib/person'
 describe Person do
   context 'we create the person object and test it ' do
     let(:person) { Person.new(24, 'Nasir') }
+    let(:book) { Book.new('C++', 'Nasir') }
+    let(:date) { '30/10/2023' }
+    let(:rental) { Rental.new(date, book, person) }
     it 'Person name must be Nasir' do
       expect(person.name).to eq('Nasir')
     end
@@ -15,6 +18,9 @@ describe Person do
 
     it 'Teacher can_use_services must be true' do
       expect(person.can_use_services?).to eq(true)
+    end
+    it 'Add rentals must work prperly' do
+      expect(person.add_rental(date, book)).to be_a(Rental)
     end
   end
 end
